edf1a09df8489a57c21d462d9e9261772033b1fc
Petroid2470 <65362523+Petroid2470@users.noreply.github.com>
2025-09-28 01:44:34 +0200

    Moved config to .env, added IPv6 support, automatic Record IDs

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
-# Simple-DDNS-Script
-A simple ddns script i made for cloudflare, thats helpful a lot when u dont have a static ip.
+# CFDDNS
 
-### On the script itself there are instructions.
+Simple DDNS script using Cloudflare API to update a domain's IPv4 and IPv6 addresses.
 
+## How to use
 
-### ⬇️Other helpful information⬇️
+Download the ddns script (ddns.sh), download .env.example and rename it to .env (in the same folder)
+Run ```chmod +x ddns.sh``` to make the script executable
+Run the script
 
-* __How to get the ZONE_ID for your A record.__
+Reccomended: Add the script to your cronjobs for automatic updating.
 
-`curl -s -X GET "https://api.cloudflare.com/client/v4/zones/YOUR_ZONE_ID/dns_records?type=A" -H "Authorization: Bearer YOUR_API_TOKEN" -H "Content-Type: application/json" | jq -r '.result[] | "\(.name) => \(.id)"'`
+## Helpful information
 
-It should give output in your bash like this.
+How to get API token: [Cloudflare Documentation](https://developers.cloudflare.com/fundamentals/api/get-started/create-token/)
+How to get Account and Zone ID: [Cloudflare Documentation](https://developers.cloudflare.com/fundamentals/account/find-account-and-zone-ids/)
 
-`text.lol => (Zone Id) aaa.text.lol => (Zone Id) bbb.text.lol => (Zone Id)`
-
-* __Don't forget to make it executable by doing: `chmod +x ddns.sh` on your bash.__
-
-* __TIP: Automate this script with cronjob so you don't have to manually run it everytime!__
-
-### Any other questions? Add me on discord, "the_real_astr4x"!
+IPv6, .env and automatic Record IDs made by Petroid
--- ddns.sh
+++ ddns.sh
@@ -1,42 +1,80 @@
 #!/bin/bash
+set -e
+# Gets Cloudflare API stuff from .env
+source .env
 
-# Your cloudflare information
-API_TOKEN="Your api tokken"
-ZONE_ID="Your zone id"
-RECORD_ID="Your record id"
-DOMAIN="It can also be subdomain ofc"
-
-# A simple txt file that will store your last ip so its no needed for cloudflare api to get triggered for no reason if your ip hasnt changed
-IP_FILE="/home/astr4x/last_ip.txt"  # (thats an example, please change the path to where u want to save the "last_ip" file
-
-# Grabbing your current ip
-IP=$(curl -s http://checkip.amazonaws.com)
-
-# If cant find ip, exit
-if [[ -z "$IP" ]]; then
-  echo "Can't find ip!"
+# Gets the IP with IPify, any other service will do if you want to change it
+IPV4=$(curl -s http://api.ipify.org || true)
+IPV6=$(curl -s http://api6.ipify.org || true)
+# If cant find neither of the IPs, exit
+if [[ -z "$IPV4" && -z "$IPV6" ]]; then
+  echo "Can't find any IP, script will exit."
   exit 1
 fi
 
-# If there is ip but it didnt changed, stop
-if [[ -f "$IP_FILE" ]]; then
-  LAST_IP=$(cat "$IP_FILE")
-  if [[ "$IP" == "$LAST_IP" ]]; then
-    echo "Ip didnt changed ($IP), no need for update!"
-    exit 0
+# Get record IDs for IPv4 and IPv6 with the domain
+RECORD_ID_V4=$(curl -s "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=a" \
+  -H "Authorization: Bearer $API_TOKEN" | jq -r --arg domain "$DOMAIN" '.result[] | select(.name==$domain) | .id')
+
+RECORD_ID_V6=$(curl -s "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=aaaa" \
+  -H "Authorization: Bearer $API_TOKEN" | jq -r --arg domain "$DOMAIN" '.result[] | select(.name==$domain) | .id')
+
+# If cant find neither of the Record IDs, exit
+if [[ -z "$RECORD_ID_V4" && -z "$RECORD_ID_V6" ]]; then
+  echo "Couldn't find any Record IDs for $DOMAIN. Check the DOMAIN entry in the .env or create an A or AAAA record in the Cloudflare Dashboard."
+  exit 1
+fi
+#IPv4 Update
+ if [[ -n "$IPV4" ]]; then
+  if [[ "$LAST_IPV4" != "$IPV4" ]]; then
+    if [[ -n "$RECORD_ID_V4" ]]; then
+      RESPONSE_IPV4=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID_V4"\
+      -H "Authorization: Bearer $API_TOKEN" \
+      -H "Content-Type: application/json" \
+      --data "{\"type\":\"A\",\"name\":\"$DOMAIN\",\"content\":\"$IPV4\",\"ttl\":1,\"proxied\":$PROXIED}")
+      if echo "$RESPONSE_IPV4" | grep -q "\"success\":true"; then
+        sed -i "s/^LAST_IPV4=.*/LAST_IPV4=\"$IPV4\"/" .env
+        echo "The IPv4 address ($IPV4) was updated on the domain $DOMAIN in Cloudflare succesfully."
+      else
+        echo "Can't update IPv4!. Response: $RESPONSE_IPV4"
+      fi
+      else
+  echo "IPv4 Record ID for $DOMAIN couldn't be found. Check the DOMAIN entry in the .env or create an A record in the Cloudflare Dashboard for that domain."
+    fi
+  else 
+   echo "Same IPv4 as before ($IPV4), no need to update."
   fi
+ else
+  echo "Couldn't get IPv4 address."
+  sed -i "s/^LAST_IPV6=.*/LAST_IPV6=\"$IPV6\"/" .env
 fi
 
-# Updating cloudflare, DO NOT change "$ZONE_ID" here cause we put the variables in the start
-RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID"\
-  -H "Authorization: Bearer $API_TOKEN" \
-  -H "Content-Type: application/json" \
-  --data "{\"type\":\"A\",\"name\":\"$DOMAIN\",\"content\":\"$IP\",\"ttl\":1,\"proxied\":false}")
 
-# If new ip updated sucessfully, save it to the "last_ip" file
-if echo "$RESPONSE" | grep -q "\"success\":true"; then
-  echo "$IP" > "$IP_FILE"
-  echo "The ip $IP updated on the domain $DOMAIN in cloudflare succesfully"
-else
-  echo "Can't update!. Response: $RESPONSE"
+#IPv6 update
+ if [[ -n "$IPV6" ]]; then
+  if [[ "$LAST_IPV6" != "$IPV6" ]]; then
+    if [[ -n "$RECORD_ID_V6" ]]; then
+      RESPONSE_IPV6=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID_V6"\
+      -H "Authorization: Bearer $API_TOKEN" \
+      -H "Content-Type: application/json" \
+      --data "{\"type\":\"AAAA\",\"name\":\"$DOMAIN\",\"content\":\"$IPV6\",\"ttl\":1,\"proxied\":$PROXIED}")
+      if echo "$RESPONSE_IPV6" | grep -q "\"success\":true"; then
+        sed -i "s/^LAST_IPV6=.*/LAST_IPV6=\"$IPV6\"/" .env
+        echo "The IPv6 address ($IPV6) was updated on the domain $DOMAIN in Cloudflare succesfully."
+      else
+        echo "Can't update IPv6!. Response: $RESPONSE_IPV6"
+      fi
+      else
+  echo "IPv6 Record ID for $DOMAIN couldn't be found. Check the DOMAIN entry in the .env or create an AAAA record in the Cloudflare Dashboard for that domain."
+    fi
+  else 
+   echo "Same IPv6 as before ($IPV6), no need to update."
+  fi
+ else
+  echo "Couldn't get IPv6 address."
+  sed -i "s/^LAST_IPV6=.*/LAST_IPV6=\"$IPV6\"/" .env
 fi
+
+# Im the god of spaghetti code and nested if statements
+# Its not good but it works
+# --Petroid
