8207142b86bcda739b95b4a2adb8e250a3d68121
Petroid2470 <281235073+Petroid2470@users.noreply.github.com>
2026-05-20 22:36:59 +0200

    stupid useless modifications to the script

--- ddns.sh
+++ ddns.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
-
 # Gets Cloudflare API stuff from .env
 cd "$(dirname "$0")" || exit 1
 source .env
-
 # Gets the IP with IPify, any other service will do if you want to change it
 IPV4=$(curl -s http://api.ipify.org || true)
 IPV6=$(curl -s http://api6.ipify.org || true)
@@ -14,69 +12,43 @@ if [[ -z "$IPV4" && -z "$IPV6" ]]; then
   exit 1
 fi
 
-# Get record IDs for IPv4 and IPv6 with the domain
-RECORD_ID_V4=$(curl -s "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=a" \
-  -H "Authorization: Bearer $API_TOKEN" | jq -r --arg domain "$DOMAIN" '.result[] | select(.name==$domain) | .id')
+# Get record IDs with the domain
+get_record() {
+curl -s "https://api.cloudflare.com/client/v4/zones/$1/dns_records?type=$2" -H "Authorization: Bearer $API_TOKEN"| jq -r --arg domain "$DOMAIN" '.result[] | select(.name==$domain) | .id'
+}
+RECORD_ID_V4=$(get_record $ZONE_ID "A")
+RECORD_ID_V6=$(get_record $ZONE_ID "AAAA")
 
-RECORD_ID_V6=$(curl -s "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=aaaa" \
-  -H "Authorization: Bearer $API_TOKEN" | jq -r --arg domain "$DOMAIN" '.result[] | select(.name==$domain) | .id')
-
-# If cant find neither of the Record IDs, exit
 if [[ -z "$RECORD_ID_V4" && -z "$RECORD_ID_V6" ]]; then
-  echo "Couldn't find any Record IDs for $DOMAIN. Check the DOMAIN entry in the .env or create an A or AAAA record in the Cloudflare Dashboard."
+  echo -e "Couldn't find any Record IDs for $DOMAIN.\nCheck the DOMAIN entry in the .env or create an A and/or AAAA record in the Cloudflare Dashboard."
   exit 1
 fi
-#IPv4 Update
- if [[ -n "$IPV4" ]]; then
-  if [[ "$LAST_IPV4" != "$IPV4" ]]; then
-    if [[ -n "$RECORD_ID_V4" ]]; then
-      RESPONSE_IPV4=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID_V4"\
-      -H "Authorization: Bearer $API_TOKEN" \
+
+update_record() {
+ if [[ -n "$2" ]]; then
+  if [[ "$3" != "$2" ]]; then
+    if [[ -n "$5" ]]; then
+      RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$6/dns_records/$5"\
+      -H "Authorization: Bearer $4" \
       -H "Content-Type: application/json" \
-      --data "{\"type\":\"A\",\"name\":\"$DOMAIN\",\"content\":\"$IPV4\",\"ttl\":1,\"proxied\":$PROXIED}")
-      if echo "$RESPONSE_IPV4" | grep -q "\"success\":true"; then
-        sed -i "s/^LAST_IPV4=.*/LAST_IPV4=\"$IPV4\"/" .env
-        echo "The IPv4 address ($IPV4) was updated on the domain $DOMAIN in Cloudflare succesfully."
+      --data "{\"type\":\"$9\",\"name\":\"$7\",\"content\":\"$2\",\"ttl\":1,\"proxied\":$8}")
+      if echo "$RESPONSE" | grep -q "\"success\":true"; then
+        sed -i "s/^LAST_$1=.*/LAST_$1=\"$2\"/" .env
+        echo "The $1 address ($2) was updated on the domain $7 in Cloudflare succesfully."
       else
-        echo "Can't update IPv4!. Response: $RESPONSE_IPV4"
+        echo "Can't update $1!. Response: $RESPONSE"
       fi
       else
-  echo "IPv4 Record ID for $DOMAIN couldn't be found. Check the DOMAIN entry in the .env or create an A record in the Cloudflare Dashboard for that domain."
+  echo "$1 Record ID for $7 couldn't be found. Check the DOMAIN entry in the .env or create an $9 record in the Cloudflare Dashboard for that domain."
     fi
   else 
-   echo "Same IPv4 as before ($IPV4), no need to update."
+   echo "Same $1 as before ($2), no need to update."
   fi
  else
-  echo "Couldn't get IPv4 address."
-  sed -i "s/^LAST_IPV4=.*/LAST_IPV4=\"$IPV4\"/" .env
+  echo "Couldn't get $1 address."
+  sed -i "s/^LAST_$1=.*/LAST_$1=\"$2\"/" .env
 fi
 
-
-#IPv6 update
- if [[ -n "$IPV6" ]]; then
-  if [[ "$LAST_IPV6" != "$IPV6" ]]; then
-    if [[ -n "$RECORD_ID_V6" ]]; then
-      RESPONSE_IPV6=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID_V6"\
-      -H "Authorization: Bearer $API_TOKEN" \
-      -H "Content-Type: application/json" \
-      --data "{\"type\":\"AAAA\",\"name\":\"$DOMAIN\",\"content\":\"$IPV6\",\"ttl\":1,\"proxied\":$PROXIED}")
-      if echo "$RESPONSE_IPV6" | grep -q "\"success\":true"; then
-        sed -i "s/^LAST_IPV6=.*/LAST_IPV6=\"$IPV6\"/" .env
-        echo "The IPv6 address ($IPV6) was updated on the domain $DOMAIN in Cloudflare succesfully."
-      else
-        echo "Can't update IPv6!. Response: $RESPONSE_IPV6"
-      fi
-      else
-  echo "IPv6 Record ID for $DOMAIN couldn't be found. Check the DOMAIN entry in the .env or create an AAAA record in the Cloudflare Dashboard for that domain."
-    fi
-  else 
-   echo "Same IPv6 as before ($IPV6), no need to update."
-  fi
- else
-  echo "Couldn't get IPv6 address."
-  sed -i "s/^LAST_IPV6=.*/LAST_IPV6=\"$IPV6\"/" .env
-fi
-
-# Im the god of spaghetti code and nested if statements
-# Its not good but it works
-# --Petroid
+}
+update_record "IPV4" "$IPV4" "$LAST_IPV4" "$API_TOKEN" "$RECORD_ID_V4" "$ZONE_ID" "$DOMAIN" "$PROXIED" "A"
+update_record "IPV6" "$IPV6" "$LAST_IPV6" "$API_TOKEN" "$RECORD_ID_V6" "$ZONE_ID" "$DOMAIN" "$PROXIED" "AAAA"
